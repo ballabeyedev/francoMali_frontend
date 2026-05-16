62458e7fa955e1c2158bc98014439b0ea42843bf
Balla221 <balla.beye_220540@groupeiam.com>
2026-05-16 14:59:24 +0000

    feat: initialize AppRouter with route definitions and authentication redirects

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Dashboard from "../pages/dashboard/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRouter() {
   return (
@@ -9,12 +10,22 @@ export default function AppRouter() {
 
         {/* redirection page racine */}
         <Route path="/" element={<Navigate to="/francomaliship/auth/login" />} />
+        <Route path="/francomaliship" element={<Navigate to="/francomaliship/auth/login" />} />
+        <Route path="/francomaliship/auth" element={<Navigate to="/francomaliship/auth/login" />} />
+
 
         {/* login */}
         <Route path="/francomaliship/auth/login" element={<Login />} />
 
         {/* dashboard */}
-        <Route path="/francomaliship/dashboard" element={<Dashboard />} />
+        <Route
+          path="/francomaliship/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
 
       </Routes>
     </BrowserRouter>
